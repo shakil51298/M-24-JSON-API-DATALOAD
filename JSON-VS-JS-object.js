b195fa26bc69c5d56b929019b2a1281bd77222c1
shakil51298 <zikaiwen02@gmail.com>
2021-02-04 00:12:32 +0800

    stringify,JSON.parse()

--- JSON-VS-JS-object.js
+++ JSON-VS-JS-object.js
@@ -1,5 +1,7 @@
-const user = {id: 10413, name:'shakil khan'};
-         const userJson = JSON.stringify(user);
-         const userFromJson = JSON.parse(userJson);
-         console.log("javascript Object",userJson);
-         console.log('JSON :',userFromJson);
+const user = {
+    id: 10413,name: 'shakil khan',friendsAge:[12, 25, 30, 40],friendsName :['shakil', 'shakib', 'azad']
+};
+const userJson = JSON.stringify(user);
+const userFromJson = JSON.parse(userJson);
+console.log("javascript Object", userJson);
+console.log('JSON :', userFromJson);
